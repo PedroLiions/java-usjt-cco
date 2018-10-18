44d5ac0d4ffcfa3365f9b2041ed7e7cccc948c42
Alunos <Alunos@BUTWKSLI3708.animaedu.intranet>
2018-10-18 20:28:58 -0300

    Crud pais

---
+++ src/service/PaisService.java
@@ -0,0 +1,32 @@
+package service;
+
+import dao.PaisDAO;
+import model.Pais;
+
+public class PaisService {
+	PaisDAO dao = new PaisDAO();
+	
+	public int criar(Pais pais) {
+		return dao.criar(pais);
+	}
+	
+	public void atualizar(Pais pais){
+		dao.atualizar(pais);
+	}
+	
+	public void excluir(int id){
+		dao.excluir(id);
+	}
+	
+	public Pais carregar(int id){
+		return dao.carregar(id);
+	}
+	
+	public Pais getMaiorPopulacao() {
+		return dao.getMaiorPopulacao();
+	}
+	
+	public Pais getMenorArea() {
+		return dao.getMenorArea();
+	}
+}
